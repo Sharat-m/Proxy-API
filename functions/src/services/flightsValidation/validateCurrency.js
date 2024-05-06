6cbb07fd9a13c0c659f2b0d80602c07d4b3ec2a9
sharat <sharat@farefirst.com>
2024-05-06 16:08:30 +0530

    validation- working of currency

--- functions/src/services/flightsValidation/validateCurrency.js
+++ functions/src/services/flightsValidation/validateCurrency.js
@@ -1,9 +1,21 @@
+const currencyData = require("../../data/currencies.json").currencies;
+
 function validateCurrency(query) {
   //############ CURRENCY ####################
-  const currency = query.currency;
-  if (!currency) {
+  const currencyCode = query.currency;
+  // console.log("currencyCode :", currencyCode);
+  if (!currencyCode) {
     return { error: true, code: 3, message: "The currency is missing" };
   }
+  //checking the Currency Code present in the currencies.json file
+  const currencyExist = currencyData.some(
+    (currency) => currency.code === currencyCode
+  );
+  // console.log("currencyExist :", currencyExist);
+  if (!currencyExist) {
+    return { error: true, code: 3, message: "The currency is invalid" };
+  }
+
   return { error: false }; //Indicates the currency is valid
 }
 module.exports = validateCurrency;
