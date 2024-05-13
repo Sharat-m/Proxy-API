462d8ff83e724d88c39c7a46e253c017fd71bf4d
sharat <sharat@farefirst.com>
2024-05-13 14:10:22 +0530

    created the json for poll request

--- functions/src/api/flights/poll.js
+++ functions/src/api/flights/poll.js
@@ -12,17 +12,31 @@ pollRouter.post("/flights/live/search/:poll", async (req, res) => {
     const docRef = db.collection("flight_details").doc(token);
     let flightDetail = await docRef.get();
     // Fetching the data from database
-    // let response = flightDetail.data();
-    //  console.log(response);
+    let response = flightDetail.data();
+     console.log(response);
+     let tripType = response.trip;
+     console.log("tripType:", tripType );
     if (flightDetail.exists) {
-      const jsonData = await fsReadFileToJSON("./src/data/poll.json");
-      return res.status(200).send({
-        sessionToken: token,
-        status: "RESULT_STATUS_COMPLETE",
-        action: "RESULT_ACTION_REPLACED",
-        // msg: response,
-        content: jsonData,
-      });
+      if(tripType === 1 ){
+        const jsonData = await fsReadFileToJSON("./src/data/one-way-poll.json");
+        return res.status(200).send({
+          sessionToken: token,
+          status: "RESULT_STATUS_COMPLETE 1",
+          action: "RESULT_ACTION_REPLACED",
+          // msg: response,
+          content: jsonData,
+        });
+      } else if(tripType === 2){
+        const jsonData = await fsReadFileToJSON("./src/data/two-way-poll.json");
+        return res.status(200).send({
+          sessionToken: token,
+          status: "RESULT_STATUS_COMPLETE 2",
+          action: "RESULT_ACTION_REPLACED",
+          // msg: response,
+          content: jsonData,
+        });
+      }
+     
     } else {
       return res.status(400).send({
         code: 3,
