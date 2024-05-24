cb6f51b9dd668c63fd64290177a97b9f1ef43fb0
sharat <sharat@farefirst.com>
2024-05-24 17:44:04 +0530

    Added the validation for the market API

--- functions/src/api/culture/markets.js
+++ functions/src/api/culture/markets.js
@@ -1,9 +1,32 @@
 const express = require("express");
 const marketsData = require("../../data/markets.json"); 
+const localeData = require("../../data/locales.json").locales;
 
 const marketsRouter = express.Router();
 
-marketsRouter.get("/markets/locale", (req, res) => {
+function validateGeoLocale(locales){
+  if(!locales){
+    return { error: true, code: 3, message: "The locale is missing" };
+  }
+  const localeExist = localeData.some((locale) => locale.code === locales );
+if(!localeExist) {
+  return { error:true, code: 3, message: " The locale is invalid" };
+}
+return { error : false };
+
+}
+
+
+marketsRouter.get("/markets/:locale?", (req, res) => {
+  let locales = req.params.locale;
+  let validationLocale = validateGeoLocale (locales);
+  if(validationLocale.error){
+    return res.status(400).json({
+      code: validationLocale.code,
+      message: validationLocale.message,
+      details:[],
+    })
+  }
   try {
     res.status(200).send(marketsData);
   } catch (error) {
