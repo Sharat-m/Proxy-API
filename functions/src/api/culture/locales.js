ad991e0c0f1348425aaddc3fe6c5ec61327dda51
sharat <sharat@farefirst.com>
2024-05-28 15:33:11 +0530

    Removed the ip validation API and Added the regex for IP validation

--- functions/src/api/culture/locales.js
+++ functions/src/api/culture/locales.js
@@ -4,7 +4,20 @@ const localesData = require("../../data/locales.json");
 const localesRouter = express.Router();
 
 
+
 localesRouter.get("/locales", (req, res) => {
+  // let url = req.url;
+  // console.log(url);
+  // if (url == "locales"){
+  //   console.log("enternred");
+  // return  res.status(400).json({
+  //     code: 5,
+  //     message: "Not Found",
+  //     details:[],
+  //   })
+  // }
+
+
   try {
     res.status(200).send(localesData);
   } catch (error) {
--- functions/src/api/ip/ip-result.js
+++ functions/src/api/ip/ip-result.js
@@ -1,27 +1,29 @@
 const express = require("express");
-const axios = require("axios");
 const ipData = require("../../data/ipresult.json");
 
 const ipResultRouter = express.Router();
 
-function validateIp(ipAddress, status) {
+function validateIp(ipAddress) {
+  // Regex expression for validating IPv4
+  let ipv4 =
+    /^(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9][0-9]|[0-9])\.(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9][0-9]|[0-9])\.(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9][0-9]|[0-9])\.(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9][0-9]|[0-9])$/;
+  // Regex expression for validating IPv6
+  let ipv6 = /^(([0-9a-fA-F]{1,4}):){7}([0-9a-fA-F]{1,4})$/;
+
   if (!ipAddress) {
     return { error: true, code: 3, message: "The ip address is missing" };
   }
-  if (status == "fail") {
+
+  if (!ipAddress.match(ipv4) && !ipAddress.match(ipv6)) {
     return { error: true, code: 3, message: "The ip address is invalid" };
   }
-
   return { error: false };
 }
 
 ipResultRouter.get("/culture/nearestculture", async (req, res) => {
   let ipAddress = req.query.ipAddress;
 
-  const response = await axios.get(`http://ip-api.com/json/${ipAddress}`);
-  let status = response.data.status;
-
-  let valiadationIp = validateIp(ipAddress, status);
+  let valiadationIp = validateIp(ipAddress);
   if (valiadationIp.error) {
     return res.status(400).json({
       code: valiadationIp.code,
